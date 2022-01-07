6562e720bb6dbabcdd6daf898489391a1598e205
Pavel Che <alien.killer3@bk.ru>
2022-01-07 16:10:45 +0300

    the Header is styled and Header-slider implemented

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function () {
     $('.header-slider').slick({
-        
+        dots: true,
+        vertical: true,
+        prevArrow: '<button type="button" class="slick-prev slick-arrow"><img src="images/prev.svg" alt="Arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="images/next.svg" alt="Arrow"></button>',
     })
 });
 
