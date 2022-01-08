96727524e8e107424862f2bf469d22b8ba01cdd7
Pavel Che <alien.killer3@bk.ru>
2022-01-08 15:41:34 +0300

    we connected the slider 'product__name' to the slider 'product__content'

--- js/main.js
+++ js/main.js
@@ -2,9 +2,27 @@ $(function () {
     $('.header-slider').slick({
         dots: true,
         vertical: true,
-        prevArrow: '<button type="button" class="slick-prev slick-arrow"><img src="images/prev.svg" alt="Arrow"></button>',
-        nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="images/next.svg" alt="Arrow"></button>',
-    })
+        prevArrow: '<button type="button" class="header-prev header-arrow"><img src="images/prev.svg" alt="Arrow"></button>',
+        nextArrow: '<button type="button" class="header-next header-arrow"><img src="images/next.svg" alt="Arrow"></button>',
+    });
+
+
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        asNavFor: '.product__content',
+        focusOnSelect: true,
+        vertical: true,
+        prevArrow: '<button type="button" class="product-prev product-arrow"><img src="images/product-prev.svg" alt="Arrow"></button>',
+        nextArrow: '<button type="button" class="product-next product-arrow"><img src="images/product-next.svg" alt="Arrow"></button>',
+    });
+    $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        vertical: true,
+        arrows: false,
+    });
 });
 
 
