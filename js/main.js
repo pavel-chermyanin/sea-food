0a61afcb4cdb2b3e1f0e94ecd3e914af192d66ab
Pavel Che <alien.killer3@bk.ru>
2022-01-11 20:15:58 +0300

    first screen adaptived

--- js/main.js
+++ js/main.js
@@ -1,34 +1,44 @@
 $(function () {
-    $('.header-slider').slick({
+    $(".header-slider").slick({
         dots: true,
         vertical: true,
-        prevArrow: '<button type="button" class="header-prev header-arrow"><img src="images/prev.svg" alt="Arrow"></button>',
-        nextArrow: '<button type="button" class="header-next header-arrow"><img src="images/next.svg" alt="Arrow"></button>',
+        prevArrow:
+            '<button type="button" class="header-prev header-arrow"><img src="images/prev.svg" alt="Arrow"></button>',
+        nextArrow:
+            '<button type="button" class="header-next header-arrow"><img src="images/next.svg" alt="Arrow"></button>',
+        responsive: [
+            {
+                breakpoint: 361,
+                settings: {
+                    dots: false,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                },
+            },
+        ],
     });
 
-
-    $('.product__name').slick({
+    $(".product__name").slick({
         slidesToShow: 6,
         slidesToScroll: 1,
-        asNavFor: '.product__content',
+        asNavFor: ".product__content",
         focusOnSelect: true,
         vertical: true,
-        prevArrow: '<button type="button" class="product-prev product-arrow"><img src="images/product-prev.svg" alt="Arrow"></button>',
-        nextArrow: '<button type="button" class="product-next product-arrow"><img src="images/product-next.svg" alt="Arrow"></button>',
+        prevArrow:
+            '<button type="button" class="product-prev product-arrow"><img src="images/product-prev.svg" alt="Arrow"></button>',
+        nextArrow:
+            '<button type="button" class="product-next product-arrow"><img src="images/product-next.svg" alt="Arrow"></button>',
     });
-    $('.product__content').slick({
+    $(".product__content").slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        asNavFor: '.product__name',
+        asNavFor: ".product__name",
         vertical: true,
         arrows: false,
     });
 
-    $('.menu__btn').on('click', function () {
-        $('.menu__list').toggleClass('menu__list--active');
-    })
+    $(".menu__btn").on("click", function () {
+        $(".menu__list").toggleClass("menu__list--active");
+    });
 });
-
-
-
-
